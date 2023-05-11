194b2a191c4608f1d993cd36b5f88eda3b127105
Jack Griffiths <jack.griffiths@outlook.com>
2023-05-11 13:37:52 +0100

    Use CSS Nesting (with help from PostCSS)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import postcssNesting from "postcss-nesting";
 
 export default defineConfig({
     plugins: [
         solidPlugin()
-    ]
+    ],
+    css: {
+        postcss: {
+            plugins: [
+                postcssNesting()
+            ]
+        }
+    }
 });
