627fd818f3a10e4745f199ecbbd4c5691865755e
Jack Griffiths <jack.griffiths@outlook.com>
2023-05-26 12:53:02 +0100

    Added placeholder "recycle bin" to improve UX.
    When a placeholder is removed, its details (such as default value) are temporarily kept in a "recycle bin" so that those details can be reinstated if the placeholder is re-added.

--- src/snippets/snippet-store.ts
+++ src/snippets/snippet-store.ts
@@ -1,18 +1,36 @@
 import { evaluate } from "./analysis/evaluator";
 import { TokenKind, parse } from "./analysis/parser";
-import { defaultDelimiter, Language, reservedPlaceholders, Snippet, SnippetKind, SnippetType } from "./snippet-model";
+import { defaultDelimiter, Language, Placeholder, reservedPlaceholders, Snippet, SnippetKind, SnippetType } from "./snippet-model";
 import { batch, createMemo } from "solid-js";
 import { createStore, produce } from "solid-js/store";
 
 export default function createSnippetStore(createDefault: () => Snippet) {
+    // Create the reactive signal for the snippet.
     const [snippet, set] = createStore<Snippet>(createDefault());
     const setWith = (func: (snippet: Snippet) => void) => set(produce(func));
 
+    // Set up derived logic.
     const canHaveNamespaces = () => snippet.language === Language.CSharp || snippet.language === Language.VisualBasic;
     const parsedTokens = createMemo(() => parse(snippet.code, snippet.delimiter || defaultDelimiter));
     const isValidCode = () => parsedTokens().find(i => i.kind === TokenKind.BadToken) === undefined;
     const preview = createMemo(() => evaluate(snippet, parsedTokens()));
 
+    // While editing the snippet's code, it's quite easy to briefly lose a placeholder.
+    // For example, to replace the method name of this code "public int Add(int $x1$, int $x2$) {}"
+    // with a placeholder, typing $ before Add would briefly cause a placeholder of "$Add(int $" to be
+    // parsed. Therefore $x1$ and $x2$ would be removed from the model. Only once the closing $ was
+    // added after Add would they reappear. This could be annoying if details such as the default values
+    // for $x1$ and $x2$ has already been entered. To fix this, we can use a recycle bin to reinstate
+    // deleted placeholders.
+    const placeholderRecycleBin = new Map<string, Placeholder>();
+
+    // This is used when opening up a snippet from a file or when doing "New".
+    const replace = (snippet: Snippet) => {
+        set(snippet);
+        placeholderRecycleBin.clear();
+    };
+
+    // Reparse the placeholders in the code block and synchronize the model.
     const updatePlaceholders = () => {
         const [added, removed] = reparsePlaceholders(snippet);
 
@@ -22,15 +40,16 @@ export default function createSnippetStore(createDefault: () => Snippet) {
         }
 
         setWith(s => {
+            // Evaluate what needs to be done to the model.
             const toRetain = s.placeholders.filter(i => !removed.has(i.name));
-            const toAdd = Array.from(added).map(i => ({
-                name: i,
-                defaultValue: "",
-                function: "",
-                tooltip: "",
-                isEditable: true,
-            }));
+            const toRemove = s.placeholders.filter(i => removed.has(i.name));
+            const toAdd = Array.from(added).map(i => placeholderRecycleBin.get(i) || createPlaceholder(i));
 
+            // Update the recycle bin.
+            toAdd.forEach(i => placeholderRecycleBin.delete(i.name));
+            toRemove.forEach(i => placeholderRecycleBin.set(i.name, i));
+
+            // Update the model.
             s.placeholders = toRetain;
             s.placeholders.push(...toAdd);
             s.placeholders.sort((a, b) => a.name.localeCompare(b.name));
@@ -42,8 +61,8 @@ export default function createSnippetStore(createDefault: () => Snippet) {
         canHaveNamespaces,
         isValidCode,
         snippetOps: {
-            new: () => set(createDefault()),
-            replace: (snippet: Snippet) => set(snippet),
+            new: () => replace(createDefault()),
+            replace,
             preview,
             updateTitle: (title: string) =>
                 set("title", title),
@@ -101,6 +120,16 @@ export default function createSnippetStore(createDefault: () => Snippet) {
                     setWith(s => { s.namespaces.splice(index, 1); }),
             }
         }
+    };
+}
+
+function createPlaceholder(name: string): Placeholder {
+    return {
+        name: name,
+        defaultValue: "",
+        function: "",
+        tooltip: "",
+        isEditable: true,
     };
 }
 
